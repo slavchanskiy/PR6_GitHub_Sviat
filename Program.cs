0fc33da43968d2a9cb7dfb1d11ded8c938ad0ffc
dimitri <odv.0379@gmail.com>
2024-03-30 18:49:53 +0300

    commit 5

--- Program.cs
+++ Program.cs
@@ -4,21 +4,10 @@ class Program
 {
     static void Main()
     {
-        // Вводим координаты трех точек
-        Console.WriteLine("Введите координаты первой точки (x1 y1): ");
-        string[] input1 = Console.ReadLine().Split(' ');
-        double x1 = double.Parse(input1[0]);
-        double y1 = double.Parse(input1[1]);
-
-        Console.WriteLine("Введите координаты второй точки (x2 y2): ");
-        string[] input2 = Console.ReadLine().Split(' ');
-        double x2 = double.Parse(input2[0]);
-        double y2 = double.Parse(input2[1]);
-
-        Console.WriteLine("Введите координаты третьей точки (x3 y3): ");
-        string[] input3 = Console.ReadLine().Split(' ');
-        double x3 = double.Parse(input3[0]);
-        double y3 = double.Parse(input3[1]);
+        // Задаем координаты трех точек
+        double x1 = 3, y1 = 3;
+        double x2 = 5, y2 = 3;
+        double x3 = 4, y3 = 5;
 
         // Вычисляем расстояния между точками
         double d1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
